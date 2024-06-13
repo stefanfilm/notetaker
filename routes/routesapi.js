3fe8bff60821cf2b32a10190214c3ef1dae77470
Stefan Wanigatunga <stefan.wanigatunga@gmail.com>
2024-06-12 21:47:29 -0700

    Adds delete functionality to the cards

--- routes/routesapi.js
+++ routes/routesapi.js
@@ -12,18 +12,34 @@ router.post("/notes", async (req,res) => {
     const notes = await fs.readFile(dbpath, {encoding:"utf-8"})
     console.log(notes)
     const updatedNotes = JSON.parse(notes)
-    updatedNotes.push(req.body)
+    let assignId = 1;
+    if (updatedNotes.length > 0) {
+        assignId = updatedNotes [updatedNotes.length-1].id+1
+    }
+    const noteObject = {
+    id : assignId,
+    ...req.body
+    }
+    updatedNotes.push(noteObject)
     console.log(updatedNotes)
     await fs.writeFile(dbpath,JSON.stringify(updatedNotes))
     res.json(notes)
 })
 
-router.delete("/notes", async (req,res) => {
+router.delete("/notes/:id", async (req,res) => {
+    const noteId = req.params.id
+    console.log(noteId)
     const dbpath = (path.join (__dirname,"../db/db.json"))
     const notes = await fs.readFile(dbpath, {encoding:"utf-8"})
-    const updatedNotes = 
-    await fs.writeFile(dbpath,JSON.stringify(updatedNotes))
-    res.json(notes)
-}
+    const updatedNotes = JSON.parse(notes)
+    const filterNotes = updatedNotes.filter ((item) => {
+        console.log(item.id, noteId) 
+        return item.id != noteId
+
+    })
+    console.log(filterNotes)
+    await fs.writeFile(dbpath,JSON.stringify(filterNotes))
+    res.json(filterNotes)
+})
 
 module.exports = router
