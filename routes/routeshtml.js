f06dada1b6997e024bdcda67467b1fde031a6a46
Stefan Wanigatunga <stefan.wanigatunga@gmail.com>
2024-06-17 12:05:34 -0700

    changes routes and moves Index

--- routes/routeshtml.js
+++ routes/routeshtml.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const router = require("express").Router()
 router.get("/", (req,res) => {
-    res.sendFile(path.join (__dirname,"../public/index.html"))
+    res.sendFile(path.join (__dirname,"../index.html"))
 })
 router.get("/notes", (req,res) => {
     res.sendFile(path.join (__dirname, "../public/notes.html"))
